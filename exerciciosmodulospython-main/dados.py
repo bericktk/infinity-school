b4cf3d46be897cb4a44fcc6724dacf6ad75155b0
Bruno Erick <141072611+bericktk@users.noreply.github.com>
2024-08-14 18:49:03 -0300

    Add files via upload

---
+++ exerciciosmodulospython-main/dados.py
@@ -0,0 +1,9 @@
+import random
+
+def lancar_dados():
+    dado1 = random.randint(1, 6)
+    dado2 = random.randint(1, 6)
+
+    valor_total = dado1 + dado2
+    return valor_total
+
---
+++ exerciciosmodulospython-main/main.py
@@ -0,0 +1,5 @@
+from dados import lancar_dados
+
+resultado = lancar_dados()
+
+print(f'O resultado do lançamento dos dados é: {resultado}')
